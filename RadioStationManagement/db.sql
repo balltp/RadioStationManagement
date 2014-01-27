38c188f4d5a4ddfe2a9b2a87ea34dfc4a307aecb
ronsc <fdroncpe17@gmail.com>
2014-01-28 01:44:54 +0700

    # เพิ่มผู้ใช้งานใหม่

--- RadioStationManagement/db.sql
+++ RadioStationManagement/db.sql
@@ -16,28 +16,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 -- 
--- โครงสร้างตาราง `fileupload`
--- 
-
-CREATE TABLE `fileupload` (
-  `id` int(5) unsigned zerofill NOT NULL auto_increment,
-  `upload_by` varchar(20) collate utf8_unicode_ci NOT NULL,
-  `upload_name` text collate utf8_unicode_ci NOT NULL,
-  `upload_file` text collate utf8_unicode_ci NOT NULL,
-  `upload_date` enum('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday') collate utf8_unicode_ci NOT NULL,
-  `upload_time` text collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=15 ;
-
--- 
--- dump ตาราง `fileupload`
--- 
-
-INSERT INTO `fileupload` VALUES (00013, 'admin', 'KasetTalk', 'KasetTalk-22325.mp3', 'Sunday', '15.00');
-
--- --------------------------------------------------------
-
--- 
 -- โครงสร้างตาราง `member`
 -- 
 
@@ -48,7 +26,7 @@ CREATE TABLE `member` (
   `name` varchar(30) collate utf8_unicode_ci NOT NULL,
   `level` enum('ADMIN','USER') collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 -- 
 -- dump ตาราง `member`
@@ -57,6 +35,8 @@ CREATE TABLE `member` (
 INSERT INTO `member` VALUES (00001, 'admin', '1', 'มทส.', 'ADMIN');
 INSERT INTO `member` VALUES (00002, 'ronsc', '1', 'รณกร', 'USER');
 INSERT INTO `member` VALUES (00003, 'test1', '1', 'เอิร์ธ', 'USER');
+INSERT INTO `member` VALUES (00004, 'monx', '345', 'เอิร์ธ', 'ADMIN');
+INSERT INTO `member` VALUES (00006, 'test2', '1', 'รณกร จันทร์นาลาว', 'USER');
 
 -- --------------------------------------------------------
 
@@ -75,10 +55,12 @@ CREATE TABLE `user_upload` (
   `List` varchar(100) NOT NULL,
   `FilesPath` varchar(100) NOT NULL,
   PRIMARY KEY  (`FilesID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- 
 -- dump ตาราง `user_upload`
 -- 
 
 INSERT INTO `user_upload` VALUES (00001, 'ronsc', 'KasetTalk-21929.MP3', 6187620, 'audio/mp3', 'Monday', '12.00', 'KasetTalk', 'files/');
+INSERT INTO `user_upload` VALUES (00002, 'admin', 'Janmo-233818.mp3', 3808570, 'audio/mp3', 'Monday', '15.00', '30mSUT', '/files/');
+INSERT INTO `user_upload` VALUES (00003, 'admin', 'Janmo-234727.mp3', 6187620, 'audio/mp3', 'Tuesday', '19.00', '30mSUT', '/files/');
