6c6dcfadbfd6b644f1f6296fccbdf0813fff3174
ronsc <fdroncpe17@gmail.com>
2014-02-02 17:10:57 +0700

    แก้ไข Icon ลบ,ฟังเพลง

--- RadioStationManagement/lib/config.php
+++ RadioStationManagement/lib/config.php
@@ -1,7 +1,7 @@
 <?php
 	define("DB_HOST","localhost");
 	define("DB_ROOT", "root");
-	define("DB_PW", "12345");
+	define("DB_PW", "6368109");
 	define("DB_NAME", "db_radio");
 	define("USER", $_SESSION['USER']);
 	define("IP", $_SERVER["REMOTE_ADDR"]);
--- RadioStationManagement/views/list.php
+++ RadioStationManagement/views/list.php
@@ -31,8 +31,8 @@
 					<td><?php echo $data['DayTime']; ?></td>
 					<td><?php echo $data['List']; ?></td>
 					<td>
-					<a href="index.php?v=play&name=<?php echo $data['FilesName'];?>" class = "btn btn-success btn-xs">ฟังเพลง </a>
-					<button onClick="JavaScript:window.location='index.php?v=Delete&ID=<?php echo $data['FilesID']?>'" class="btn btn-xs btn-danger">ลบ</button>
+					<a href="index.php?v=play&name=<?php echo $data['FilesName']; ?>" class = "btn btn-success btn-xs"><span class="glyphicon glyphicon-music"></span></a>
+					<a href="index.php?v=Delete&ID=<?php echo $data['FilesID']; ?>" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-trash"></span></a>
 					</td>
 				
 				</tr>
