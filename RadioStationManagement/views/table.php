02b48b8744a5820b8cdc6221ea71d89b4a6b9aae
ronsc <fdroncpe17@gmail.com>
2014-03-01 02:14:03 +0700

    # ตารางเวลาบอกว่า วัน - เวลาใด มีการอัพไฟล์แล้ว

--- RadioStationManagement/views/table.php
+++ RadioStationManagement/views/table.php
@@ -4,16 +4,24 @@
 	
 	$File = new File();
 
-	
 	$DayOfWeek = array(
-					'1' => array('0' => 'อาทิตย์', '1' => 'Sunday'),
-					'2' => array('0' => 'จันทร์', '1' => 'Monday'),
-					'3' => array('0' => 'อังคาร', '1' => 'Tuesday'),
-					'4' => array('0' => 'พุธ', '1' => 'Wednesday'),
-					'5' => array('0' => 'พฤหัสบดี', '1' => 'Thursday'),
-					'6' => array('0' => 'ศุกร์', '1' => 'Friday'),
-					'7' => array('0' => 'เสาร์', '1' => 'Saturday'),
+					'1' => array('0' => 'อาทิตย์', 'Sunday' => '1'),
+					'2' => array('0' => 'จันทร์', 'Monday' => '2'),
+					'3' => array('0' => 'อังคาร', 'Tuesday' => '3'),
+					'4' => array('0' => 'พุธ', 'Wednesday' => '4'),
+					'5' => array('0' => 'พฤหัสบดี', 'Thursday' => '5'),
+					'6' => array('0' => 'ศุกร์', 'Friday' => '6'),
+					'7' => array('0' => 'เสาร์', 'Saturday' => '7'),
 				);
+	$DayOfWeekEN = array(
+			'Sunday' => '1',
+			'Monday' => '2',
+			'Tuesday' => '3',
+			'Wednesday' => '4',
+			'Thursday' => '5',
+			'Friday' => '6',
+			'Saturday' => '7',
+	);
 	
 	$TimeOfDay = array(
 					'1' => '06.00',
@@ -30,13 +38,43 @@
 					'12' => '17.00',
 					'13' => '18.00',
 					'14' => '19.00',
-				);
+	);
+	
+	$TimeOfDayEN = array(
+			'06.00' => '1',
+			'07.00' => '2',
+			'08.00' => '3',
+			'09.00' => '4',
+			'10.00' => '5',
+			'11.00' => '6',
+			'12.00' => '7',
+			'13.00' => '8',
+			'14.00' => '9',
+			'15.00' => '10',
+			'16.00' => '11',
+			'17.00' => '12',
+			'18.00' => '13',
+			'19.00' => '14',
+	);
 	
 	$db = new DB();
-	$sql = "SELECT DayTime, DayWeek FROM user_upload";
+	$user = USER;
+	if($user != "admin"){
+		$sql = "SELECT DayTime, DayWeek FROM user_upload WHERE Name='$user'";
+	}else{
+		$sql = "SELECT DayTime, DayWeek FROM user_upload";
+	}
+	
 	$db->query($sql);
 	$ArrData = $db->fetch_array();
-	echo print_r($ArrData);
+
+	$x = 0; 
+	$y = 0;
+	foreach($ArrData as $a){
+		$xy[$x++] = $DayOfWeekEN[$a['DayWeek']];
+		$xy[$x++] = $TimeOfDayEN[$a['DayTime']];
+	}
+	
 ?>
 	
 <div class="panel panel-primary">
@@ -47,15 +85,12 @@
 		<table class="table table-bordered" width="800" align="center" border="1">
 			<?php 
 			for($i=0;$i<=count($TimeOfDay);$i++){
-			?>
-			<?php 
-			if($TimeOfDay[$i]=='12.00'){
+				if($TimeOfDay[$i]=='12.00'){
 			?>
 			<tr bgcolor="#DDDDD">
 			<?php }else{?>
 			<tr>
-			<?php }?>
-				<?php 
+			<?php } 
 				for($j=0;$j<=count($DayOfWeek);$j++){
 					if($i==0 && $j==0){?>
 				<td bgcolor="#DDDDD" width="100px">
@@ -74,10 +109,17 @@
 				<?php }else{?>
 				<td align="center">
 					<?php 
-					
-					echo $DayOfWeek[$i][$j]."<br>".$TimeOfDay[$i];
-							//echo "<br>".$ArrData[0][DayWeek]; 
-							?>
+						for($k=0;$k<count($xy);$k+=2){
+							if($xy[$k]==$j && $xy[$k+1]==$i){
+					?>
+					<a class="btn btn-info">
+						<span class="glyphicon glyphicon-ok-sign"></span>
+						&nbsp;มีไฟล์
+					</a>
+					<?php 	
+							}
+						}
+					?>
 				</td>
 				<?php }}?>
 			</tr>
